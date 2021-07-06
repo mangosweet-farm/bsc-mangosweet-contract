94a76d00a491ef3c2d6383c8b9c06c914906b612
pat-mangosweet <munkunsri@gmail.com>
2021-07-06 22:38:16 +0700

    modify deployment files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
-var Mangosweet = artifacts.require("./Mangosweet.sol");
+var MangoSweet = artifacts.require("./MangoSweet.sol");
 
 module.exports = function(deployer) {
   // Deploy Mangosweet Token
-  deployer.deploy(Mangosweet);
+  deployer.deploy(MangoSweet, 1000);
 
 };
