0a9b524c68147f746e2d9431198321ee81ade026
TanapongR <zensesage@gmail.com>
2021-09-02 13:10:46 +0700

    Add landing page

---
+++ client/build.sh
@@ -0,0 +1,3 @@
+npm run build
+cp -a src/images build
+http-server build
--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import { Helmet } from 'react-helmet'
+import SimpleStorageContract from "./contracts/MangoSweet.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
+
+const TITLE = 'MANGO SWEET'
 
 class App extends Component {
   state = { storageValue: 0, web3: null, accounts: null, contract: null };
@@ -22,10 +25,11 @@ class App extends Component {
         SimpleStorageContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
-
+      
+      const wallet = accounts[0].slice(0,7) + '...' + accounts[0].slice(accounts[0].length - 6, accounts[0].length - 1);
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      this.setState({ web3, accounts, contract: instance, wallet }, this.runExample);
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -39,32 +43,51 @@ class App extends Component {
     const { accounts, contract } = this.state;
 
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    // await contract.methods.set(5).send({ from: accounts[0] });
 
     // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+    // const response = await contract.methods.get().call();
 
     // Update state with the result.
-    this.setState({ storageValue: response });
+    // this.setState({ storageValue: response });
   };
+
+  getWalletAddress = async () => {
+    const accounts = this.state.accounts + '';
+    let wallet = accounts.slice(0,7) + '' + accounts.slice(accounts.length - 5, accounts.length - 1);
+    return wallet;
+  }
 
   render() {
     if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+      return <div>
+        <Helmet>
+          <title>{ TITLE }</title>
+        </Helmet>
+        Connecting...
+        </div>;
     }
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+      <div>
+        <Helmet>
+          <title>{ TITLE }</title>
+        </Helmet>
+        <div class="wallet">
+          <div class="address">Your wallet: {this.state.wallet}</div>
+
+        </div>
+        <div class="landing">
+          <label class="label-header">We’re coming soon</label>
+          <div class="mango">
+            <img  src="images/mango.png" />
+          </div>
+          <label class="label-domain">MangoSweet.finance</label>
+          <label class="label-description">Stay Connected</label>
+          <div class="icon-social">
+            <img class="icon" src="images/twitter.png" />
+            <img class="icon" src="images/telegram.png" />
+          </div>
+        </div>
       </div>
     );
   }
